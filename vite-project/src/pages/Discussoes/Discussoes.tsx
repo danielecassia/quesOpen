f834f30756ebec3df6ac43c5ca35fb59f57c19cc
Daniele Cassia <danielecassia9@gmail.com>
2022-05-31 12:04:34 -0300

    base tela de listar discussoes da disciplina

--- vite-project/src/pages/Discussoes/Discussoes.tsx
+++ vite-project/src/pages/Discussoes/Discussoes.tsx
@@ -1,16 +1,24 @@
+import { Box, Stack, Skeleton } from "@mui/material";
+import React, { useState } from "react";
+import Post from "../Post/Post";
 import ResponsiveAppBar from '../components/Navbar/Navbar';
 
-export function Discussoes() {
+export function DiscussoesTeste(){
 
-    return(
-        <div>
-            <div>
-            <ResponsiveAppBar/>
-            </div>
-            <div>
-                testeeeee
-            </div>
-        </div>
+  return (
+    <div>
+      <ResponsiveAppBar/>
+      <Box flex={3} p={{ xs: 0, md: 2 }}>
+          <Stack spacing={1}>
+          </Stack>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+      </Box>
 
-    );
-}
+    </div>
+  );
+};
