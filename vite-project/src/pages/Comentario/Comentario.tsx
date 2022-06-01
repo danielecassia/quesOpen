820e06f5610bba56fec0792af4587cd070c5ee0a
Daniele Cassia <danielecassia9@gmail.com>
2022-06-01 13:38:32 -0300

    teste modal

--- vite-project/src/pages/Comentario/Comentario.tsx
+++ vite-project/src/pages/Comentario/Comentario.tsx
@@ -1,36 +1,114 @@
-import { Favorite, MoreVert } from "@mui/icons-material";
-import {
-  Avatar,
-  Card,
-  CardContent,
-  CardHeader,
-  IconButton,
-  Typography,
-} from "@mui/material";
+import React, { useState, useEffect } from 'react'
+import ModalBase from '../components/Modal/ModalBase';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup'
 
-
-export function Comentario() {
-  return (
-    <Card sx={{ margin: 2 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: "gray" }} aria-label="recipe">
-                C
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVert />
-              </IconButton>
-            }
-            title='Comentário'
-            subheader='sei la'
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              testeeeeeeeeeeeeeeeeeeee
-            </Typography>
-          </CardContent>
-      </Card>
-  );
+const defaultInputValues = {
+    userId: '',
+    email: '',
+    phoneNumber: ''
 };
+
+const Comentario = ({ open, onClose, addNewUser }) => {
+    const [values, setValues] = useState(defaultInputValues);
+
+    const modalStyles = {
+        inputFields: {
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '20px',
+            marginBottom: '15px',
+            '.MuiFormControl-root': {
+                marginBottom: '20px',
+            },
+        },
+    };
+
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
+    const validationSchema = Yup.object().shape({
+        userId: Yup.string()
+            .required('User ID is required')
+            .min(6, 'User ID must be at least 6 characters'),
+        email: Yup.string()
+            .required('Email is required')
+            .email('Email is invalid.'),
+        phoneNumber: Yup.string()
+            .matches(phoneRegExp, 'Phone number is not valid'),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const addUser = (data) => {
+        addNewUser(data);
+    };
+
+    const handleChange = (value) => {
+        setValues(value)
+    };
+
+    useEffect(() => {
+        if (open) setValues(defaultInputValues);
+    }, [open])
+
+    const getContent = () => (
+        <Box sx={modalStyles.inputFields}>
+            <TextField
+                placeholder="User ID"
+                name="userId"
+                label="User ID"
+                required
+                {...register('userId')}
+                error={errors.userId ? true : false}
+                helperText={errors.userId?.message}
+                value={values.userId}
+                onChange={(event) => handleChange({ ...values, userId: event.target.value })}
+            />
+            <TextField
+                placeholder="Email"
+                name="email"
+                label="Email"
+                required
+                {...register('email')}
+                error={errors.email ? true : false}
+                helperText={errors.email?.message}
+                value={values.email}
+                onChange={(event) => handleChange({ ...values, email: event.target.value })}
+            />
+            <TextField
+                placeholder="Phone number"
+                name="phoneNumber"
+                label="Phone number"
+                required
+                {...register('phoneNumber')}
+                error={errors.phoneNumber ? true : false}
+                helperText={errors.phoneNumber?.message}
+                value={values.phoneNumber}
+                onChange={(event) => handleChange({ ...values, phoneNumber: event.target.value })}
+            />
+        </Box>
+    );
+    
+    return (
+        <ModalBase
+            open={open}
+            onClose={onClose}
+            title="New user"
+            subTitle="Fill out inputs and hit 'submit' button."
+            content={getContent()}
+            onSubmit={handleSubmit(addUser)}
+        />
+            
+    )
+}
+
+export default Comentario
