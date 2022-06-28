6dce909033fb5a95273e48861838fc19be0bcfc7
Henrique César <rickcesaralves07@gmail.com>
2022-06-28 15:17:31 -0300

    Correção bd

--- api-node/database/index.js
+++ api-node/database/index.js
@@ -2,10 +2,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  // process.env.DB_NAME,
-  "projeto_api_pt",
-  // process.env.DB_USER,
-  "root",
+  process.env.DB_NAME,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
