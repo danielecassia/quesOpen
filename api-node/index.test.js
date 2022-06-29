332ed0491a42b690df81257ebef90f15322db1fd
Arthur Codama <arthur_codama@hotmail.com>
2022-06-28 21:30:25 -0300

    Alterando testes de usuario

--- api-node/index.test.js
+++ api-node/index.test.js
@@ -3,27 +3,34 @@ const usuario = require('./usuario/service/usuarioService.js');
 const usuarioRepositoryMockup = require('./usuario/repository/usuarioRepositoryMockup.js');
 
 test('getAllUsuarios', async (t) => {
+    oldRepository = usuario.usuarioRepository
     usuario.usuarioRepository = usuarioRepositoryMockup;
 
     t.equal(typeof (await usuario.getAllUsuarios()), Array, "tipo ok")
     t.equal((await usuario.getAllUsuarios()).length, 2, "tamanho ok")
+    
+    usuario.usuarioRepository = oldRepository
     t.end()  
 });
 
 test('getUsuarioById', async (t) => {
+    oldRepository = usuario.usuarioRepository
     usuario.usuarioRepository = usuarioRepositoryMockup;
 
     t.equal((await usuario.getUsuarioById(1)).nome_usuario, "Arthur")
     t.equal((await usuario.getUsuarioById(2)).nome_usuario, "Henrique")
+
+    usuario.usuarioRepository = oldRepository
     t.end()  
 });
 
 test('createUser', async (t) => {
+    oldRepository = usuario.usuarioRepository
     usuario.usuarioRepository = usuarioRepositoryMockup;
     newUser = {
-        nome: "Arthur2",
-        email: "art2@email.com",
-        email_confirm: "art2@email.com",
+        nome: "Arthur3",
+        email: "art3@email.com",
+        email_confirm: "art3@email.com",
         data_nasc: "2000-01-01",
         senha: "coxinha123",
         senha_confirm: "coxinha123"
@@ -31,14 +38,12 @@ test('createUser', async (t) => {
 
 
     t.equals((await usuario.createUser(newUser)), [3, 1], "retorno correto")
-    t.equals((await usuario.getUsuarioById(3)).email, "art2@email.com", "usuario adicionado")
+    t.equals((await usuario.getUsuarioById(3)).email, "art3@email.com", "usuario adicionado")
     usuario.deletarUsuario(3);
+
+    usuario.usuarioRepository = oldRepository
     t.end()  
 });
-
-// createUser = async (user) => {
-//     return usuarioRepository.create(user);
-// };
 
 // userbyEmail = async(emailUsuario) => {
 //     return usuarioRepository.findByEmail(emailUsuario);
