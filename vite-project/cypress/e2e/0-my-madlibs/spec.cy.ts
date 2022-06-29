7dd5f4493b769e7197eafb7f8c299cc2fb4e1fbb
Daniele Cassia <danielecassia9@gmail.com>
2022-06-29 15:13:21 -0300

    fix: add teste e2e

--- vite-project/cypress/e2e/0-my-madlibs/spec.cy.ts
+++ vite-project/cypress/e2e/0-my-madlibs/spec.cy.ts
@@ -1,12 +1,17 @@
 describe('The MadLibs Main Form', () => {
   it('Criar uma Discussão', () => {
     cy.login('dani@dani', '123')
-    cy.wait(2000)
-    cy.criar('Teste Cypress', 'Isso é um teste legal de automatização de testes')
-  });
-  it('Fazer comentário', () => {
+    cy.wait(3000)
     cy.visit('localhost:3000/home/discussao/acessaDiscussao/5')
+    cy.wait(5000)
+    // cy.contains('AddCommentIcon').click()
     cy.comentar('Comentário da Dani')
+    cy.wait(3000)
+    cy.visit('localhost:3000/home/discussao/2')
+    cy.wait(3000)
+    cy.criar('Teste Cypress', 'Isso é um teste legal de automatização de testes')
+    cy.wait(1000)
+    cy.visit('localhost:3000/home/discussao/2')
   });
 
 })
